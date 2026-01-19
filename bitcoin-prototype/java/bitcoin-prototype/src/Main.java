1ff11dedf076d7389046977982e913d50ce59642
CUSTcoding <custodiotitossetitosse@gmail.com>
2026-01-19 15:56:32 -0600

    test(java): update  Main class to test Block functionality

--- bitcoin-prototype/java/bitcoin-prototype/src/Main.java
+++ bitcoin-prototype/java/bitcoin-prototype/src/Main.java
@@ -1,15 +1,43 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        List<String> transactions = new ArrayList<>();
+        transactions.add("Alice -> Bob : 10 BTC");
+        transactions.add("Bob -> Charlie : 5 BTC");
+
+
+        Block block = new Block(
+                0,
+                0,
+                "",
+                "merkle-root-fake",
+                3,
+                "0000000000000",
+                transactions
+        );
+
+
+        block.addTrasaction("Charlie -> Dave : 2 BTC");
+
+
+        System.out.println("Minerando bloco...");
+        block.mineBlock(3);
+
+
+        System.out.println("Hash calculado: " + block.calculatehash());
+
+
+        Map<String, Object> blockMap = block.toMap();
+
+        System.out.println("\n=== Block como Map ===");
+        for (Map.Entry<String, Object> entry : blockMap.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
         }
     }
 }
